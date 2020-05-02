ebadfb13b3190f58a9a4ccc0d54f149bf52f6850
Rahul Mangalampalli <rahul_mangalampalli@yahoo.in>
2020-05-02 12:59:50 +0530

    Create .caffemodel

--- create_caffemodel.py
+++ create_caffemodel.py
@@ -14,19 +14,34 @@ layers = list(data.keys())
 #Initialize prototxt file
 net = caffe.Net('inception_resnet_v1_test.prototxt',caffe.TEST)
 
+bias_a = 1
+bias_b = 1
+bias_c = 1
 # To Read all layers respective weights.
 for i in layers: 
     print(i)
-    if i == "InceptionResnetV1/Bottleneck/MatMul":
-        net.params[i][0].data[...] = data[i]['weights'].reshape(512,1792) 
-    elif(i.find('FusedBatchNorm') == -1):
-        net.params[i][0].data[...] = data[i]['weights'].transpose((3,2,0,1)) 
-    else:
-        net.params[i][0].data[...] = data[i]['scale']
-        net.params[i][1].data[...] = data[i]['bias']
-
-
-
-
+    if i.count('weights'):
+        net.params[i.strip('/weights')+'/Conv2d'][0].data[...] = data[i].transpose((3,2,0,1))
+    if i.count('BatchNorm'):
+        if i.count('beta'):
+            net.params[i.strip('/beta')+'/Scale'][0].data[...] = data[i]
+	
+        if i.count('Const'):
+            net.params[i.strip('/Const')+'/Bias'][0].data[...] = data[i] 
+        
+        if i.count('moving_mean'):
+            net.params[i.strip('/moving_mean')+'/FusedBatchNorm'][0].data[...] = data[i]
+        if i.count('moving_variance'):
+            net.params[i.strip('/moving_variance')+'/FusedBatchNorm'][0].data[...] = data[i]
+    if i.count('biases'):
+        if i.count('block35'):
+            net.params['BiasA'+str(bias_a)][0].data[...] = data[i]  
+            bias_a+=1
+        if i.count('block17'):
+            net.params['BiasB'+str(bias_b)][0].data[...] = data[i]  
+            bias_b+=1
+        if i.count('block8'):
+            net.params['BiasC'+str(bias_c)][0].data[...] = data[i]  
+            bias_c+=1                		
 # Save caffemodel.
-net.save('inception_resnet_v1.caffemodel')
+net.save('inception.caffemodel')
